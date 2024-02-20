cc42c49abd84aff2c56c63d2f61536f85238c753
notokrs <rusnoto030@gmail.com>
2024-02-20 21:02:04 +0700

    fix: change pylyzer -> ruff-lsp

--- configs/overrides.lua
+++ configs/overrides.lua
@@ -1,111 +1,111 @@
 local M = {}
 
 M.treesitter = {
-	autotag = {
-		enable = true,
-	},
-	rainbow = {
-		enable = true,
-		disable = { "html", "markdown" },
-		query = "rainbow-parens",
-	},
-	ensure_installed = {
-		-- lua & vim
-		"vim",
-		"lua",
+  autotag = {
+    enable = true,
+  },
+  rainbow = {
+    enable = true,
+    disable = { "html", "markdown" },
+    query = "rainbow-parens",
+  },
+  ensure_installed = {
+    -- lua & vim
+    "vim",
+    "lua",
 
-		-- web dev
-		"html",
-		"css",
-		"javascript",
-		"typescript",
-		"php",
-		"regex",
+    -- web dev
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "php",
+    "regex",
 
-		-- markdown
-		"markdown",
-		"markdown_inline",
+    -- markdown
+    "markdown",
+    "markdown_inline",
 
-		-- bash
-		"bash",
+    -- bash
+    "bash",
 
-		-- python
-		"python",
+    -- python
+    "python",
 
-		-- golang
-		"go",
-	},
+    -- golang
+    "go",
+  },
 }
 
 M.mason = {
-	ensure_installed = {
-		-- lua
-		"lua-language-server",
-		"stylua",
+  ensure_installed = {
+    -- lua
+    "lua-language-server",
+    "stylua",
 
-		-- web dev
-		"css-lsp",
-		"html-lsp",
-		"typescript-language-server",
-		"tailwind-language-server",
-		"prettier",
-		"svelte-language-server",
+    -- web dev
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "tailwind-language-server",
+    "prettier",
+    "svelte-language-server",
 
-		-- shell
-		"bash-language-server",
-		"beautysh",
+    -- shell
+    "bash-language-server",
+    "beautysh",
 
-		-- php
-		"intelephense",
-		"phpcbf",
-		"phpcs",
+    -- php
+    "intelephense",
+    "phpcbf",
+    "phpcs",
 
-		-- xml
-		"xmlformatter",
+    -- xml
+    "xmlformatter",
 
-		-- python
-		"pylyzer",
-		"pyink",
+    -- python
+    "ruff-lsp",
+    "pyink",
 
-		-- golang
-		"gopls",
-		"gofumpt",
-	},
+    -- golang
+    "gopls",
+    "gofumpt",
+  },
 }
 
 -- git support in nvimtree
 M.nvimtree = {
-	filters = {
-		dotfiles = true,
-	},
-	git = {
-		enable = true,
-		ignore = false,
-	},
-	renderer = {
-		highlight_git = true,
-		icons = {
-			show = {
-				git = true,
-			},
-		},
-	},
+  filters = {
+    dotfiles = true,
+  },
+  git = {
+    enable = true,
+    ignore = false,
+  },
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
 }
 
 -- blankline
 M.blankline = {
-	use_treesitter = true,
-	show_first_indent_level = true,
-	show_trailing_blankline_indent = true,
-	context_highlight_list = {
-		"TSRainbowRed",
-		"TSRainbowYellow",
-		"TSRainbowBlue",
-		"TSRainbowOrange",
-		"TSRainbowGreen",
-		"TSRainbowViolet",
-		"TSRainbowCyan",
-	},
+  use_treesitter = true,
+  show_first_indent_level = true,
+  show_trailing_blankline_indent = true,
+  context_highlight_list = {
+    "TSRainbowRed",
+    "TSRainbowYellow",
+    "TSRainbowBlue",
+    "TSRainbowOrange",
+    "TSRainbowGreen",
+    "TSRainbowViolet",
+    "TSRainbowCyan",
+  },
 }
 
 return M
